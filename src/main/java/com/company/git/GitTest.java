dc780c234c9bf3b41c76a720dacb88ef8642184d
Han <Han@abc.com>
2022-09-05 21:47:19 +0800

    first commit

---
+++ src/main/java/com/company/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.company.git;
+
+/**
+ * @Auther:hyz
+ * @Date:2022/9/5 - $(MONTH) - $(DAY) - 21:40
+ * @Description:com.company.git
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
